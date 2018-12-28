c468f9a736e059ac4ca9623cbe74d7ccd11b96dd
Heber Nunomura <heber28@gmail.com>
2018-12-28 16:13:25 -0200

    pag. inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :descartes
   get 'pesquisar_por_cep', to: 'cep#pesquisar_por_cep'
+  root :to => 'descartes#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
