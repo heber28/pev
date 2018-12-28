404288f88c00e61b4dc9cd6e6559de08d2212914
Heber Nunomura <heber28@gmail.com>
2018-12-28 16:56:24 -0200

    pagina estatica

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'page/index'
   resources :descartes
   get 'pesquisar_por_cep', to: 'cep#pesquisar_por_cep'
-  root :to => 'descartes#new'
+  root :to => 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
